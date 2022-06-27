957d260235b2731e98d6c42de37560c74e6da84d
Mo-Shoba <msmshoba1@gmail.com>
2022-06-27 22:16:01 +0200

    Edited base app. Added pickled files

--- base_app.py
+++ base_app.py
@@ -27,13 +27,38 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import streamlit as st
+import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
 
 # Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
+news_vectorizer = open("resources/vectorizer.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
+raw.head()
+
+# Splitting Data
+y = raw.sentiment.values
+
+def load_prediction_models(model_file):
+	loaded_model = joblib.load(open(os.path.join(model_file),"rb"))
+	return loaded_model
+
 
 # The main function where we will build the actual app
 def main():
@@ -46,38 +71,98 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Information","Prediction", "NLP" ]
 	selection = st.sidebar.selectbox("Choose Option", options)
+
 
 	# Building out the "Information" page
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+		st.markdown("In this app we will be classifying twitter sentiments around climate change")
 
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
+		
 	# Building out the predication page
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
+		all_ml_models = ["LogisticRegression","Linear SVC"]
+		model_choice = st.selectbox("Select Model",all_ml_models)
+
+		prediction_labels = {'negative': -1,'neutral': 0,'positive': 1,'news': 2}
+
+
 
 		if st.button("Classify"):
+			st.text("Original Text::\n{}".format(tweet_text))
 			# Transforming user input with vectorizer
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			if model_choice == 'LogisticRegression':
+				predictor = load_prediction_models("resources/log_reg.pkl")
+				prediction_fit = predictor.fit(vect_text)
+				prediction = predictor.predict(vect_text)
+				# st.write(prediction)
+			elif model_choice == 'Linear SVC':
+				predictor = load_prediction_models("resources/lsvc.pkl")
+				prediction_fit = predictor.fit(vect_text)
+				prediction = predictor.predict(vect_text)
+
+			final_result = get_key(prediction,prediction_labels)
+			st.success("Text Categorized as:: {}".format(final_result))
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+			predictor = joblib.load(open(os.path.join("resources/log_reg.pkl"),"rb"))
+			predictor_fit = predictor.fit(X_train, y_train)
+			prediction_fit = predictor_fit.predict(vect_text)
+			
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
+	
+	if selection == 'NLP':
+		st.info("Natural Language Processing of Text")
+		raw_text = st.text_area("Enter Text Here","Type Here")
+		nlp_task = ["Tokenization","Lemmatization","NER","POS Tags"]
+		task_choice = st.selectbox("Choose NLP Task",nlp_task)
+		if st.button("Analyze"):
+			st.info("Original Text::\n{}".format(raw_text))
 
+			docx = nlp(raw_text)
+			if task_choice == 'Tokenization':
+				result = [token.text for token in docx ]
+			elif task_choice == 'Lemmatization':
+				result = ["'Token':{},'Lemma':{}".format(token.text,token.lemma_) for token in docx]
+			elif task_choice == 'NER':
+				result = [(entity.text,entity.label_)for entity in docx.ents]
+			elif task_choice == 'POS Tags':
+				result = ["'Token':{},'POS':{},'Dependency':{}".format(word.text,word.tag_,word.dep_) for word in docx]
+
+			st.json(result)
+
+		if st.button("Tabulize"):
+			docx = nlp(raw_text)
+			c_tokens = [token.text for token in docx ]
+			c_lemma = [token.lemma_ for token in docx ]
+			c_pos = [token.pos_ for token in docx ]
+
+			new_df = pd.DataFrame(zip(c_tokens,c_lemma,c_pos),columns=['Tokens','Lemma','POS'])
+			st.dataframe(new_df)
+
+
+		if st.checkbox("WordCloud"):
+			c_text = raw_text
+			wordcloud = WordCloud().generate(c_text)
+			plt.imshow(wordcloud,interpolation='bilinear')
+			plt.axis("off")
+			st.pyplot()
+		st.set_option('deprecation.showPyplotGlobalUse', False)
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
